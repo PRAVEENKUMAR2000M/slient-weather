f1bbeec1fa603f8f36f3db4bdd17f2782bead606
praveenkumar <praveenmech483@gmail.com>
2024-08-21 21:39:50 +0530

    updated code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,20 +3,20 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer>
-  <div className="container px-5 py-4 mx-auto flex flex-wrap items-center lg:justify-between justify-center ">
-    <a className="flex items-center">
-      <img src="/weather.png" alt="logo" width={50} />
-      <span className="ml-2 text-xl">Silent Weather</span>
-    </a>
-    <p className="text-sm">
-      © 2023 All right reserved — 
-      
-        <a href="https://ankitjha.vercel.app" target='_blank' className='underline'> Ankit Jha</a>
-      
-    </p>
-   
-  </div>
-</footer>
+      <div className="container px-5 py-4 mx-auto flex flex-wrap items-center lg:justify-between justify-center ">
+        <a className="flex items-center">
+          <img src="/weather.png" alt="logo" width={50} />
+          <span className="ml-2 text-xl">Silent Weather</span>
+        </a>
+        <p className="text-sm">
+          © 2023 All right reserved —
+
+          <a href="https://hilarious-raindrop-78baa5.netlify.app/" target='_blank' className='underline'> Praveenkumar M</a>
+
+        </p>
+
+      </div>
+    </footer>
 
   )
 }
